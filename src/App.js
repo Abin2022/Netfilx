cea21864d47fbcb8de12c560e661c3b033936c31
Abin binu <tomailabin@gmail.com>
2023-08-20 17:49:38 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import NavBar from "./Component/Navbar/NavBar";
+import './App.css'
+import Banner from "./Component/Banner/Banner";
+import RowPost from "./Component/RowPost/RowPost";
+
+
+function App() {
+  return (
+   <>
+   <NavBar/>
+   <Banner/>
+   <RowPost/>
+   </>
+  );
+}
+
+export default App;
